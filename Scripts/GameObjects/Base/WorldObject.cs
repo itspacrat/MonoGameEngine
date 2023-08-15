57ac3603c0d9fa26db1811c3d39cf99b3f478b8f
itspacrat <blakemichaelgaynor@gmail.com>
2023-08-15 12:15:11 -0700

    fmt `WorldObject`

--- Scripts/GameObjects/Base/WorldObject.cs
+++ Scripts/GameObjects/Base/WorldObject.cs
@@ -7,26 +7,21 @@ namespace MonoGame_Core.Scripts
     {
         public RigidBody RigidBody { get { return (RigidBody)componentHandler.Get("rigidBody"); } }
         public Transform Transform { get { return (Transform)componentHandler.Get("transform"); } }
-        public SpriteRenderer SpriteRenderer{ get { return (SpriteRenderer)componentHandler.Get("spriteRenderer"); } }
+        public SpriteRenderer SpriteRenderer { get { return (SpriteRenderer)componentHandler.Get("spriteRenderer"); } }
         public CollisionHandler CollisionHandler { get { return (CollisionHandler)componentHandler.Get("collisionHandler"); } }
         public WorldObject(string texID, string name, string[] tags, Vector2 pos, byte layer) : base(name, tags)
-        {           
-            componentHandler.Add(new CollisionHandler(this));           
+        {
+            componentHandler.Add(new CollisionHandler(this));
             componentHandler.Add(new Transform(this, pos, 0, layer));
             componentHandler.Add(new RigidBody(this, RigidBody.RigidBodyType.Static));
-            componentHandler.Add(new SpriteRenderer(this, 
-                                            texID,
-                                            0));
+            componentHandler.Add(new SpriteRenderer(this, texID, 0));
         }
         public WorldObject(string texID, string name, string[] tags, Vector2 pos, byte layer, Vector2 drawArea) : base(name, tags)
         {
             componentHandler.Add(new CollisionHandler(this));
             componentHandler.Add(new Transform(this, pos, 0, layer));
             componentHandler.Add(new RigidBody(this, RigidBody.RigidBodyType.Static));
-            componentHandler.Add(new SpriteRenderer(this,
-                                            texID,
-                                            0,
-                                            drawArea));
+            componentHandler.Add(new SpriteRenderer(this, texID, 0, drawArea));
         }
     }
 }
